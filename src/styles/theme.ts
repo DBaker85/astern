b561e66a96b0665cd1766d520dfc0075b7a26f55
David Baker <davidbaker@MacBook-Pro.local>
2020-10-12 17:05:49 +0200

    brighter green

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,7 +4,7 @@ const dark: DefaultTheme = {
   background: "hsla(321, 12%, 29%, 1)",
   yellow: "hsla(44, 81%, 67%, 1)",
   light: "hsla(141, 30%, 91%, 1)",
-  green: "hsla(146, 22%, 44%, 1)",
+  green: "hsla(143, 52%, 41%, 1)",
   dark: "hsla(180, 16%, 33%, 1)",
   red: "hsla(13, 73%, 42%, 1)",
 };
