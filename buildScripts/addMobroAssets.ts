4b9e0743a1e584e08a472fca5a62193d1e0b7a78
David <badcancer22@gmail.com>
2020-10-13 17:35:21 +0200

    fix paths again

--- buildScripts/addMobroAssets.ts
+++ buildScripts/addMobroAssets.ts
@@ -4,7 +4,7 @@ import { copy } from "fs-extra";
 const copyAssets = async () => {
   try {
     const assetPath = resolve(__dirname, "..", "src", "assets", "moBro");
-    const themepath = resolve(__dirname, "..", "theme");
+    const themepath = resolve(__dirname, "..", "theme", "astern");
     await copy(assetPath, themepath);
   } catch (err) {
     console.error(err);
