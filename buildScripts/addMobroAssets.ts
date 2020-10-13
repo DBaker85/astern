2bc13f5b3b216c2a2afa5f449d2b7c697ba54be9
David <badcancer22@gmail.com>
2020-10-13 17:28:56 +0200

    rewrite scripts

--- buildScripts/addMobroAssets.ts
+++ buildScripts/addMobroAssets.ts
@@ -1,21 +1,11 @@
-import { resolve, join } from "path";
+import { resolve } from "path";
 import { copy } from "fs-extra";
-
-const readmeFilename = "Readme.md";
-const configFilename = "theme-config.json";
 
 const copyAssets = async () => {
   try {
     const assetPath = resolve(__dirname, "..", "src", "assets", "moBro");
     const themepath = resolve(__dirname, "..", "theme");
-    await copy(
-      join(assetPath, readmeFilename),
-      join(themepath, readmeFilename)
-    );
-    await copy(
-      join(assetPath, configFilename),
-      join(themepath, "astern", configFilename)
-    );
+    await copy(assetPath, themepath);
   } catch (err) {
     console.error(err);
   }
--- buildScripts/addSdk.ts
+++ buildScripts/addSdk.ts
@@ -3,7 +3,7 @@ import { readFile, writeFile } from "fs-extra";
 
 const sdkScript = async () => {
   try {
-    const themepath = resolve(__dirname, "..", "theme", "astern", "index.html");
+    const themepath = resolve(__dirname, "..", "theme", "index.html");
     const indexFile = await readFile(themepath, "utf8");
     const replacedIndex = indexFile.replace(
       "#SDK_SCRIPT#",
