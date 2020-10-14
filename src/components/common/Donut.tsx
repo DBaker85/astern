53b26b06e365ea0c3e161de52639215923646521
David Baker <davidbaker@MacBook-Pro.local>
2020-10-14 17:35:08 +0200

    add indicators

--- src/components/common/Donut.tsx
+++ src/components/common/Donut.tsx
@@ -49,20 +49,64 @@ export const Donut: FunctionComponent<DonutProps> = ({
   value,
 }) => {
   const segmentValue = mapRange(value, 0, max, 0, 100);
+  const warningValue = mapRange(warning, 0, max, 0, 100);
+  const criticalValue = mapRange(critical, 0, max, 0, 100);
+  const warningIndicator = mapRange(warning, 0, max, 0, 360);
+  const criticalIndicator = mapRange(critical, 0, max, 0, 360);
   return (
     <svg width="100%" height="100%" viewBox="0 0 42 42">
       <DonutHole cx="21" cy="21" r="15.91549430918954" />
       <DonutRing cx="21" cy="21" r="15.91549430918954" strokeWidth="3" />
+      <circle
+        cx="21"
+        cy="21"
+        r="19.0986"
+        stroke="#fff"
+        fill="transparent"
+        strokeDasharray="1 119"
+        strokeWidth="3"
+        strokeDashoffset="90"
+      />
+      <circle
+        cx="21"
+        cy="21"
+        r="19.0986"
+        stroke="#fff"
+        fill="transparent"
+        style={{
+          transformOrigin: "center center",
+          transform: `rotate(${warningIndicator}deg)`,
+        }}
+        strokeDasharray="1 119"
+        strokeWidth="3"
+        strokeDashoffset="90"
+      />
+      <circle
+        cx="21"
+        cy="21"
+        r="19.0986"
+        stroke="#fff"
+        fill="transparent"
+        style={{
+          transformOrigin: "center center",
+          transform: `rotate(${criticalIndicator}deg)`,
+        }}
+        strokeDasharray="1 119"
+        strokeWidth="3"
+        strokeDashoffset="90"
+      />
+
       <DonutSegment
         cx="21"
         cy="21"
         r="15.91549430918954"
         strokeWidth="5"
         segmentValue={segmentValue}
-        warning={warning}
-        critical={critical}
+        warning={warningValue}
+        critical={criticalValue}
         strokeDashoffset="75"
       />
+
       <g className="chart-text">
         <text x="50%" y="50%" className="chart-number">
           {value}
