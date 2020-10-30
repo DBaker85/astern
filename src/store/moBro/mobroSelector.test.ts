000524ee23ebc3cbda454b9e7f588623bd96771d
David <badcancer22@gmail.com>
2020-10-30 21:10:40 +0100

    add tests

---
+++ src/store/moBro/mobroSelector.test.ts
@@ -0,0 +1,41 @@
+import {
+  processorNameSelector,
+  processorCoreCountSelector,
+  gpuNameSelector,
+  processorLimitsSelector,
+  gpuLimitsSelector,
+  totalRamSelector,
+  initSelector,
+  tempAsFarenHeightSelector,
+} from "./mobroSelectors";
+
+import {
+  cpuName,
+  gpuName,
+  mocks,
+  cpuLimits,
+  gpuLimits,
+  ramCapacity,
+  coreCount,
+} from "./mocks";
+
+describe("selectors", () => {
+  it("should get cpu name", () => {
+    expect(processorNameSelector(mocks)).toBe(cpuName);
+  });
+  it("should get core count", () => {
+    expect(processorCoreCountSelector(mocks)).toEqual(coreCount);
+  });
+  it("should get gpu name", () => {
+    expect(gpuNameSelector(mocks)).toBe(gpuName);
+  });
+  it("should get processor limits", () => {
+    expect(processorLimitsSelector(mocks)).toBe(cpuLimits);
+  });
+  it("should get gpu limits", () => {
+    expect(gpuLimitsSelector(mocks)).toBe(gpuLimits);
+  });
+  it("should get ram capacity", () => {
+    expect(totalRamSelector(mocks)).toBe(ramCapacity.totalcapacityGb);
+  });
+});
---
+++ src/store/moBro/mocks/index.ts
@@ -0,0 +1,98 @@
+import { RootStateType } from "../../index";
+
+export const cpuName = "AmazeCpu";
+export const gpuName = "AmazeRayTracing";
+export const ramCapacity = {
+  bytes: 17179869184,
+  totalcapacityGb: "16gb",
+  count: 2,
+  capacityunit: "B",
+};
+export const cpuLimits = {
+  hardwaretype: "Processor",
+  warning: 70,
+  critical: 80,
+  max: 100,
+};
+export const gpuLimits = {
+  hardwaretype: "Graphics",
+  warning: 80,
+  critical: 100,
+  max: 110,
+};
+
+export const coreCount = 4;
+
+const coreSensors = Array(coreCount).fill({
+  sensortype: "Usage",
+  label: "CPU Core #1",
+});
+
+export const mocks: RootStateType = {
+  moBro: {
+    sensors: {
+      openhardwaremonitor: {
+        data: [
+          {
+            hardwaretype: "Processor",
+            title: cpuName,
+
+            sensors: coreSensors,
+          },
+
+          {
+            hardwaretype: "Graphics",
+            title: gpuName,
+          },
+        ],
+      },
+      librehardwaremonitor: {
+        data: [
+          {
+            hardwaretype: "Processor",
+            title: cpuName,
+
+            sensors: [
+              {
+                sensortype: "Usage",
+
+                label: "CPU Core #1",
+              },
+              {
+                sensortype: "Usage",
+
+                label: "CPU Core #2",
+              },
+              {
+                sensortype: "Usage",
+
+                label: "CPU Core #3",
+              },
+              {
+                sensortype: "Usage",
+
+                label: "CPU Core #4",
+              },
+            ],
+          },
+
+          {
+            hardwaretype: "Graphics",
+            title: "NVIDIA GeForce GTX 1080 Ti",
+          },
+        ],
+      },
+    },
+    hardware: {
+      memory: ramCapacity,
+    },
+    settings: {
+      temperatureAsFahrenheit: false,
+      hardware: {
+        temperature: [cpuLimits, gpuLimits],
+      },
+    },
+    loading: false,
+    init: true,
+  },
+};
