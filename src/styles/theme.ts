1b14299a7a5dd0a2939af62a5dcf162ea7c96049
David Baker <davidbaker@MacBook-Pro.local>
2020-11-30 18:09:10 +0100

    add ui/mdx

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,15 @@
 import { DefaultTheme } from "styled-components";
 
 const dark: DefaultTheme = {
-  darkBackground: "hsla(212,10%,19%, 1)", //
-  background: "hsla(213,10%,16%, 1)", //
   yellow: "hsla(36, 99%, 47%, 1)",
-  light: "hsla(212, 9%, 88%, 1)", //
-  green: "hsla(88, 60%, 50%, 1)", //
-  dark: "hsla(210, 10%, 14%, 1)", //
-  red: "hsla(352, 55%, 42%, 1)", //
+
+  green: "hsla(88, 60%, 50%, 1)",
+
+  red: "hsla(352, 55%, 42%, 1)",
+  dark: "hsla(210, 10%, 14%, 1)",
+  darkBackground: "hsla(212,10%,19%, 1)",
+  background: "hsla(213,10%,16%, 1)",
+  light: "hsla(212, 9%, 88%, 1)",
 };
 
 export { dark };
