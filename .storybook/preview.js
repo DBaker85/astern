fa0789827569f4e50fe7d56cc041981d92563790
David Baker <davidbaker@MacBook-Pro.local>
2020-12-04 17:26:43 +0100

    add clock functionality

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
-import { addDecorator } from '@storybook/react';
+import { addDecorator } from "@storybook/react";
 
 import { globalStyle } from "../src/styles";
 import { dark } from "../src/styles/theme";
-import { withPerformance } from 'storybook-addon-performance';
-
+import { withPerformance } from "storybook-addon-performance";
 
 const GlobalStyle = createGlobalStyle`${globalStyle}`;
 const theme = dark;
@@ -13,6 +12,12 @@ const theme = dark;
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: { expanded: true },
+  options: {
+    storySort: {
+      order: ["Example", "UI", "Common", "Pages"],
+      method: "alphabetical",
+    },
+  },
 };
 
 export const decorators = [
--- src/components/common/clock/clock.tsx
+++ src/components/common/clock/clock.tsx
@@ -1,3 +1,21 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 
-export const Clock: FunctionComponent = () => <div>17:30</div>;
+export const Clock: FunctionComponent = () => {
+  const [time, setTime] = useState(new Date());
+  const [dots, setDots] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(() => new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <span>{time.getHours()}</span>
+      <span style={{ opacity: dots ? 1 : 0 }}>:</span>
+      <span>{time.getMinutes().toString().padStart(2, "0")}</span>
+    </div>
+  );
+};
