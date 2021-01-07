1ed7978e866c5965e7772379a2ec31025af00903
David Baker <davidbaker@MacBook-Pro.local>
2021-01-07 12:02:52 +0100

    add screenshot task

---
+++ buildScripts/fetchScreenshots.ts
@@ -0,0 +1,74 @@
+import { request, gql } from "graphql-request";
+import { red, green, yellow } from "chalk";
+import { createWriteStream } from "fs-extra";
+import { resolve, join } from "path";
+import { get } from "https";
+
+type ChromaticTest = {
+  thumbnailUrl: string;
+  spec: {
+    component: {
+      displayName: string;
+      name: string;
+    };
+  };
+};
+
+type ChromaticResponse = {
+  app: {
+    lastBuild: {
+      tests: ChromaticTest[];
+    };
+  };
+};
+
+const docsFolder = resolve(__dirname, "..", "src", "docs");
+
+const screenshot = async () => {
+  const query = gql`
+    {
+      app(id: "5fcf5f3ca2b6ec002176eeef") {
+        lastBuild {
+          tests {
+            thumbnailUrl
+            spec {
+              component {
+                displayName
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+  try {
+    const chromaticData = await request<ChromaticResponse>(
+      "https://www.chromatic.com/graphql",
+      query
+    );
+    const mappedScreenshots = chromaticData.app.lastBuild.tests
+      .filter(
+        (test) =>
+          test.spec.component.displayName === "Main" ||
+          test.spec.component.displayName === "Logo"
+      )
+      .map((test) => ({
+        name: test.spec.component.displayName,
+        url: test.thumbnailUrl,
+      }))
+      .forEach((screenshot) => {
+        const fileExtension = screenshot.url.split(".").pop();
+        const fileName = `${screenshot.name}.${fileExtension}`;
+        const file = createWriteStream(join(docsFolder, fileName));
+        get(screenshot.url, function (response) {
+          response.pipe(file);
+        });
+        console.log(`${green("Created screenshot :")} > ${yellow(fileName)}`);
+      });
+  } catch (err) {
+    console.log(red(err));
+  }
+};
+
+screenshot();
