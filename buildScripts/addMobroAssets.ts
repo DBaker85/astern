02e76355b11c3c9746f4f3324210e20eea4f0bd8
David <badcancer22@gmail.com>
2021-01-19 18:38:53 +0100

    some cleanup

--- buildScripts/addMobroAssets.ts
+++ buildScripts/addMobroAssets.ts
@@ -2,12 +2,12 @@ import { resolve, join } from "path";
 import { copy } from "fs-extra";
 import { green, red } from "chalk";
 
-const readmeFilename = "Readme.md";
+// const readmeFilename = "Readme.md";
 const configFilename = "theme-config.json";
 
 const copyAssets = async () => {
   try {
-    const readmeFile = resolve(__dirname, "..", "src", "docs", readmeFilename);
+    // const readmeFile = resolve(__dirname, "..", "src", "docs", readmeFilename);
     const configFile = resolve(
       __dirname,
       "..",
@@ -16,7 +16,7 @@ const copyAssets = async () => {
       configFilename
     );
     const themepath = resolve(__dirname, "..", "theme", "astern");
-    await copy(readmeFile, join(themepath, readmeFilename));
+    // await copy(readmeFile, join(themepath, readmeFilename));
     await copy(configFile, join(themepath, configFilename));
     console.log(green("Mobro Assets added to build folder"));
   } catch (err) {
