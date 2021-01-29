5b5dd29f26acb15fd6d3b51a3e3caace3f5164cc
David Baker <davidbaker@MacBook-Pro.local>
2021-01-29 12:59:23 +0100

    add webpack for docs

---
+++ configs/common.js
@@ -0,0 +1,54 @@
+// shared config (dev and prod)
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  context: resolve(__dirname, "../docs"),
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.tsx?$/],
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          "file-loader?hash=sha512&digest=hex&name=img/[contenthash].[ext]",
+          "image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false",
+        ],
+      },
+      {
+        test: /\.pug$/,
+        use: ["pug-loader"],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[contenthash].[ext]",
+            outputPath: "fonts/",
+          },
+        },
+      },
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "index.pug" })],
+  //   externals: {
+  //     react: "React",
+  //     "react-dom": "ReactDOM",
+  //   },
+  performance: {
+    hints: false,
+  },
+};
