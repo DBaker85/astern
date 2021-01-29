8157cee277fee7377afba096c9abf53ff81df6e0
David Baker <davidbaker@MacBook-Pro.local>
2021-01-29 10:49:14 +0100

    fix builds

--- buildScripts/replaceDocPaths.ts
+++ buildScripts/replaceDocPaths.ts
@@ -1,8 +1,8 @@
-import { readFile, writeFile } from "fs-extra";
-import { resolve } from "path";
+import { readFile, writeFile, readdir } from "fs-extra";
+import { resolve, join } from "path";
 import { red, green, yellow } from "chalk";
 
-const replaceDocs = async () => {
+const replaceDocScriptPaths = async () => {
   try {
     const indexPath = resolve(__dirname, "..", ".docs", "astern", "index.html");
 
@@ -13,7 +13,7 @@ const replaceDocs = async () => {
 
     let newFile = indexFile;
 
-    foundScripts.forEach((script, index) => {
+    foundScripts.forEach((script) => {
       const srcRx = new RegExp('src="[\\d\\D]*?"', "g");
       const srcPath = script.match(srcRx)[0];
       const fixedPath = srcPath.replace('src="/', 'src="/astern/');
@@ -26,4 +26,33 @@ const replaceDocs = async () => {
   }
 };
 
-replaceDocs();
+const replaceDocAssetPaths = async () => {
+  try {
+    const buildPath = resolve(__dirname, "..", ".docs", "astern");
+
+    const directoryContents = await readdir(buildPath);
+    const matchedJsFile = directoryContents.filter((content) => {
+      return content.includes(".js");
+    });
+    const matchedFontFiles = directoryContents.filter((content) => {
+      return /([a-zA-Z0-9\s_\\.\-\(\):])+(.ttf|.woff2)$/.test(content);
+    });
+
+    const indexFilePath = join(buildPath, matchedJsFile[0]);
+
+    const indexFile = await readFile(indexFilePath, "utf8");
+
+    let newFile = indexFile;
+
+    matchedFontFiles.forEach((font) => {
+      newFile = newFile.replace(`="/${font}`, `="/astern/${font}`);
+    });
+
+    await writeFile(indexFilePath, newFile);
+  } catch (err) {
+    console.log(red(err));
+  }
+};
+
+replaceDocScriptPaths();
+replaceDocAssetPaths();
